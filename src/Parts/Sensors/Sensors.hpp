bedff01547e76d7368f7c9557daf67c49a3d0e44
Qewa2 <enthusiast-01bergkessel@icloud.com>
2025-12-03 14:35:13 +0100

    added measureRaw() to Sensor Class

--- src/Parts/Sensors/Sensors.hpp
+++ src/Parts/Sensors/Sensors.hpp
@@ -6,8 +6,9 @@
 class Sensor : public Part
 {
     public:
-        virtual void update()   = 0;
-        virtual float measure() = 0;
+        virtual void update()      = 0;
+        virtual float measure()    = 0;
+        virtual float measureRaw() = 0;
 };
 
 #endif // !SENSORS_HPP
